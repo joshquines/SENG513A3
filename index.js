b679c08cb6e6cffaeb099d6da737cbf925bace5c
Josh <josh.qns@gmail.com>
2018-03-01 11:28:12 -0700

    next

--- index.js
+++ index.js
@@ -10,8 +10,50 @@ http.listen( port, function () {
 
 app.use(express.static(__dirname + '/public'));
 
+
+// Chatroom Variables
+let numUsers = 0; // Number of users 
+let usernames = []; // All users
+let messageLog = []; // All messages
+
+
 // listen to 'chat' messages
 io.on('connection', function(socket){
+
+    // LOGIN
+    socket.on('userLogin', function(msg){
+        console.log('Entering userLogin');
+        
+
+        // Check if username not in use
+        if (usernames.indexOf(msg.toString()) == -1){
+            socket.emit('chat', "Username already in use");
+        }
+        else{
+            socket.nickname = msg.toString();
+            usernames.push(socket.nickname);
+            io.emit('chat', socket.nickname + " has joined");
+        }
+
+    })
+
+    // CHANGE USERNAME
+    socket.on('changeUser', function(nickname){
+        // Check if nickname is not in use
+        if (usernames.indexOf(nickname) == -1){
+            // Apply new username
+
+        }
+        else{
+            // Username cannot be changed
+        }
+    })
+
+    // CHANGE COLOR
+
+    // Chat Stuff
+
+
     socket.on('chat', function(msg){
 	io.emit('chat', msg);
     });
--- public/client.js
+++ public/client.js
@@ -1,12 +1,26 @@
 // shorthand for $(document).ready(...)
-$(function() {
+$(function chatHandler() {
     var socket = io();
+
+    // CHAT STUFF
     $('form').submit(function(){
-	socket.emit('chat', $('#m').val());
-	$('#m').val('');
-	return false;
+        socket.emit('chat', $('#m').val());
+        // Parse message here
+        if ($('#m').val().startsWith("/nick ")){
+            socket.emit('changeUser', "yoyoyoyo");
+        }
+        else if($('#m').val().startsWith("/nickcolor ")){
+            socket.emit('changeUserColor', "yoyoyoyo");
+        }
+        else{
+            socket.on('chat', function chatHandler(msg){
+                $('#messages').append($('<li>').text(msg + 'hahahaha'));
+        });
+    }
+    return false;
     });
-    socket.on('chat', function(msg){
-	$('#messages').append($('<li>').text(msg));
-    });
+
+    // DO LOGIN STUFF HERE
+    
 });
+
